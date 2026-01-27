d574bb2dfa7c3043b8b72f0019847e0b805a1c11
CrazyAsura <leoncdzt@gmail.com>
2026-01-26 21:15:22 -0300

    fix: update postcss config for tailwind v4

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+      origin: process.env.ALLOWED_ORIGINS?.split(',') || [],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
